25159f8a659eb08575c7c32809131e0f89b52eaa
LinkinStars <linkinstar@foxmail.com>
2023-03-15 15:37:24 +0800

    feat(connector): github connector check email verified status and upgrade github sdk

--- connector/github/github.go
+++ connector/github/github.go
@@ -8,27 +8,28 @@ import (
 
 	"github.com/answerdev/answer/plugin"
 	"github.com/answerdev/plugins/connector/github/i18n"
-	"github.com/google/go-github/v48/github"
+	"github.com/google/go-github/v50/github"
+	"github.com/segmentfault/pacman/log"
 	"golang.org/x/oauth2"
 	oauth2GitHub "golang.org/x/oauth2/github"
 )
 
-type GitHubConnector struct {
-	Config *GitHubConnectorConfig
+type Connector struct {
+	Config *ConnectorConfig
 }
 
-type GitHubConnectorConfig struct {
+type ConnectorConfig struct {
 	ClientID     string `json:"client_id"`
 	ClientSecret string `json:"client_secret"`
 }
 
 func init() {
-	plugin.Register(&GitHubConnector{
-		Config: &GitHubConnectorConfig{},
+	plugin.Register(&Connector{
+		Config: &ConnectorConfig{},
 	})
 }
 
-func (g *GitHubConnector) Info() plugin.Info {
+func (g *Connector) Info() plugin.Info {
 	return plugin.Info{
 		Name:        plugin.MakeTranslator(i18n.InfoName),
 		SlugName:    "github_connector",
@@ -39,30 +40,30 @@ func (g *GitHubConnector) Info() plugin.Info {
 	}
 }
 
-func (g *GitHubConnector) ConnectorLogoSVG() string {
+func (g *Connector) ConnectorLogoSVG() string {
 	return `PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMTIgMGMtNi42MjYgMC0xMiA1LjM3My0xMiAxMiAwIDUuMzAyIDMuNDM4IDkuOCA4LjIwNyAxMS4zODcuNTk5LjExMS43OTMtLjI2MS43OTMtLjU3N3YtMi4yMzRjLTMuMzM4LjcyNi00LjAzMy0xLjQxNi00LjAzMy0xLjQxNi0uNTQ2LTEuMzg3LTEuMzMzLTEuNzU2LTEuMzMzLTEuNzU2LTEuMDg5LS43NDUuMDgzLS43MjkuMDgzLS43MjkgMS4yMDUuMDg0IDEuODM5IDEuMjM3IDEuODM5IDEuMjM3IDEuMDcgMS44MzQgMi44MDcgMS4zMDQgMy40OTIuOTk3LjEwNy0uNzc1LjQxOC0xLjMwNS43NjItMS42MDQtMi42NjUtLjMwNS01LjQ2Ny0xLjMzNC01LjQ2Ny01LjkzMSAwLTEuMzExLjQ2OS0yLjM4MSAxLjIzNi0zLjIyMS0uMTI0LS4zMDMtLjUzNS0xLjUyNC4xMTctMy4xNzYgMCAwIDEuMDA4LS4zMjIgMy4zMDEgMS4yMy45NTctLjI2NiAxLjk4My0uMzk5IDMuMDAzLS40MDQgMS4wMi4wMDUgMi4wNDcuMTM4IDMuMDA2LjQwNCAyLjI5MS0xLjU1MiAzLjI5Ny0xLjIzIDMuMjk3LTEuMjMuNjUzIDEuNjUzLjI0MiAyLjg3NC4xMTggMy4xNzYuNzcuODQgMS4yMzUgMS45MTEgMS4yMzUgMy4yMjEgMCA0LjYwOS0yLjgwNyA1LjYyNC01LjQ3OSA1LjkyMS40My4zNzIuODIzIDEuMTAyLjgyMyAyLjIyMnYzLjI5M2MwIC4zMTkuMTkyLjY5NC44MDEuNTc2IDQuNzY1LTEuNTg5IDguMTk5LTYuMDg2IDguMTk5LTExLjM4NiAwLTYuNjI3LTUuMzczLTEyLTEyLTEyeiIvPjwvc3ZnPg==`
 }
 
-func (g *GitHubConnector) ConnectorName() plugin.Translator {
+func (g *Connector) ConnectorName() plugin.Translator {
 	return plugin.MakeTranslator(i18n.ConnectorName)
 }
 
-func (g *GitHubConnector) ConnectorSlugName() string {
+func (g *Connector) ConnectorSlugName() string {
 	return "github"
 }
 
-func (g *GitHubConnector) ConnectorSender(ctx *plugin.GinContext, receiverURL string) (redirectURL string) {
+func (g *Connector) ConnectorSender(ctx *plugin.GinContext, receiverURL string) (redirectURL string) {
 	oauth2Config := &oauth2.Config{
 		ClientID:     g.Config.ClientID,
 		ClientSecret: g.Config.ClientSecret,
 		Endpoint:     oauth2GitHub.Endpoint,
 		RedirectURL:  receiverURL,
-		Scopes:       nil,
+		Scopes:       []string{"user:email"},
 	}
 	return oauth2Config.AuthCodeURL("state")
 }
 
-func (g *GitHubConnector) ConnectorReceiver(ctx *plugin.GinContext) (userInfo plugin.ExternalLoginUserInfo, err error) {
+func (g *Connector) ConnectorReceiver(ctx *plugin.GinContext, receiverURL string) (userInfo plugin.ExternalLoginUserInfo, err error) {
 	code := ctx.Query("code")
 	// Exchange code for token
 	oauth2Config := &oauth2.Config{
@@ -72,7 +73,7 @@ func (g *GitHubConnector) ConnectorReceiver(ctx *plugin.GinContext) (userInfo pl
 	}
 	token, err := oauth2Config.Exchange(context.Background(), code)
 	if err != nil {
-		return userInfo, err
+		return userInfo, fmt.Errorf("code exchange failed: %s", err.Error())
 	}
 
 	// Exchange token for user info
@@ -83,7 +84,7 @@ func (g *GitHubConnector) ConnectorReceiver(ctx *plugin.GinContext) (userInfo pl
 	cli := github.NewClient(client)
 	resp, _, err := cli.Users.Get(context.Background(), "")
 	if err != nil {
-		return userInfo, err
+		return userInfo, fmt.Errorf("failed getting user info: %s", err.Error())
 	}
 
 	metaInfo, _ := json.Marshal(resp)
@@ -95,10 +96,37 @@ func (g *GitHubConnector) ConnectorReceiver(ctx *plugin.GinContext) (userInfo pl
 		MetaInfo:    string(metaInfo),
 		Avatar:      resp.GetAvatarURL(),
 	}
+
+	// guarantee email was verified
+	userInfo.Email = g.guaranteeEmail(userInfo.Email, token.AccessToken)
 	return userInfo, nil
 }
 
-func (g *GitHubConnector) ConfigFields() []plugin.ConfigField {
+func (g *Connector) guaranteeEmail(email string, accessToken string) string {
+	if len(email) == 0 {
+		return ""
+	}
+	client := oauth2.NewClient(context.Background(), oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: accessToken},
+	))
+	client.Timeout = 15 * time.Second
+	cli := github.NewClient(client)
+
+	emails, _, err := cli.Users.ListEmails(context.Background(), &github.ListOptions{Page: 1})
+	if err != nil {
+		log.Error(err)
+		return ""
+	}
+	for _, e := range emails {
+		if e.GetEmail() == email && e.GetVerified() {
+			log.Infof("email %s was verified", email)
+			return email
+		}
+	}
+	return ""
+}
+
+func (g *Connector) ConfigFields() []plugin.ConfigField {
 	return []plugin.ConfigField{
 		{
 			Name:        "client_id",
@@ -125,8 +153,8 @@ func (g *GitHubConnector) ConfigFields() []plugin.ConfigField {
 	}
 }
 
-func (g *GitHubConnector) ConfigReceiver(config []byte) error {
-	c := &GitHubConnectorConfig{}
+func (g *Connector) ConfigReceiver(config []byte) error {
+	c := &ConnectorConfig{}
 	_ = json.Unmarshal(config, c)
 	g.Config = c
 	return nil
--- connector/github/go.mod
+++ connector/github/go.mod
@@ -4,7 +4,8 @@ go 1.19
 
 require (
 	github.com/answerdev/answer v1.0.6-0.20230302041358-2ebf057d538d
-	github.com/google/go-github/v48 v48.2.0
+	github.com/google/go-github/v50 v50.1.0
+	github.com/segmentfault/pacman v1.0.2
 	golang.org/x/oauth2 v0.4.0
 )
 
@@ -28,7 +29,6 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
-	github.com/segmentfault/pacman v1.0.2 // indirect
 	github.com/segmentfault/pacman/contrib/i18n v0.0.0-20221219081300-f734f4a16aa0 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.1.0 // indirect
