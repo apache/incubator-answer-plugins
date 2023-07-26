5775dd94101d21ebc7929b5d4792d50c88d46ed7
LinkinStars <linkinstar@foxmail.com>
2023-07-26 18:56:06 +0800

    feat(search): add elasticsearch plugin

---
+++ search/es/es_operator.go
@@ -0,0 +1,90 @@
+package es
+
+import (
+	"context"
+	"github.com/olivere/elastic/v7"
+	"github.com/segmentfault/pacman/log"
+	"net/http"
+)
+
+// Operator elasticsearch client
+// only support basic functions, not support bulk operations
+type Operator struct {
+	C *elastic.Client
+}
+
+func NewOperator(urls []string, username, password string) (c *Operator, err error) {
+	doer := LoggingHttpClient{
+		c: http.Client{},
+	}
+	esClient, err := elastic.NewSimpleClient(
+		elastic.SetHttpClient(doer),
+		elastic.SetURL(urls...),
+		elastic.SetBasicAuth(username, password),
+		elastic.SetSniff(false),
+		elastic.SetErrorLog(NewErrLogger()))
+	if err != nil {
+		return nil, err
+	}
+	c = &Operator{C: esClient}
+	return c, nil
+}
+
+func (op *Operator) CreateIndex(ctx context.Context, indexName string, mapping string) (err error) {
+	log.Debugf("try to create index: %s", indexName)
+	exist, err := op.C.IndexExists(indexName).Do(ctx)
+	if err != nil {
+		return err
+	}
+	if exist {
+		log.Debugf("index %s already exists", indexName)
+		return nil
+	}
+	_, err = op.C.CreateIndex(indexName).BodyString(mapping).Do(ctx)
+	if err != nil {
+		log.Errorf("create index %s failed: %s", indexName, err.Error())
+		return err
+	}
+	return nil
+}
+
+func (op *Operator) QueryDoc(ctx context.Context, indexName string,
+	query elastic.Query, sort *elastic.FieldSort, cols *elastic.FetchSourceContext,
+	page, size int) (
+	result *elastic.SearchResult, err error) {
+	log.Debugf("try to query doc from index: %s, %d, %d", indexName, page, size)
+	from := (page - 1) * size
+	service := op.C.Search().Index(indexName).Query(query).From(from).Size(size)
+	if cols != nil {
+		service = service.FetchSourceContext(cols)
+	}
+	if sort != nil {
+		service = service.SortBy(sort)
+	}
+	result, err = service.Do(ctx)
+	if err != nil {
+		log.Errorf("query doc from index %s failed: %s", indexName, err.Error())
+		return nil, err
+	}
+	return result, nil
+}
+
+func (op *Operator) SaveDoc(ctx context.Context, indexName string, id string, doc interface{}) (err error) {
+	log.Debugf("try to save doc to index: %s, %s", indexName, id)
+	_, err = op.C.Update().Index(indexName).Id(id).Refresh("false").DocAsUpsert(true).Doc(doc).Upsert(doc).Do(ctx)
+	if err != nil {
+		log.Errorf("save doc to index %s failed: %s", indexName, err.Error())
+		return err
+	}
+	return nil
+}
+
+func (op *Operator) DeleteDoc(ctx context.Context, indexName string, id string) (err error) {
+	log.Debugf("try to delete doc from index: %s, %s", indexName, id)
+	_, err = op.C.Delete().Index(indexName).Id(id).Refresh("false").Do(ctx)
+	if err != nil {
+		log.Errorf("delete doc from index %s failed: %s", indexName, err.Error())
+		return err
+	}
+	return nil
+}
---
+++ search/es/go.mod
@@ -0,0 +1,49 @@
+module github.com/answerdev/plugins/search/es
+
+go 1.19
+
+require (
+	github.com/answerdev/answer v1.1.1-0.20230726083608-9655f5820511
+	github.com/olivere/elastic/v7 v7.0.32
+	github.com/segmentfault/pacman v1.0.4
+)
+
+require (
+	github.com/LinkinStars/go-i18n/v2 v2.2.2 // indirect
+	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/bytedance/sonic v1.9.1 // indirect
+	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/gin-gonic/gin v1.9.1 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.14.0 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/google/wire v0.5.0 // indirect
+	github.com/gorilla/css v1.0.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/leodido/go-urn v1.2.4 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/microcosm-cc/bluemonday v1.0.21 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/segmentfault/pacman/contrib/i18n v0.0.0-20230516093754-b76aef1c1150 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.2.11 // indirect
+	golang.org/x/arch v0.3.0 // indirect
+	golang.org/x/crypto v0.11.0 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	sigs.k8s.io/yaml v1.3.0 // indirect
+)
