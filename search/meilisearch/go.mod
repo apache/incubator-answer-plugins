326c8b246a22f4d5a20301de6d838ffecbae97fa
敬文 | 罗千洪 <jinwen.lqh@dtyunxi.com>
2023-08-18 10:03:06 +0800

    feat(plugin): add meilisearch search engine support

---
+++ search/meilisearch/go.mod
@@ -0,0 +1,18 @@
+module meilisearch
+
+go 1.21
+
+require (
+	github.com/answerdev/answer v1.1.1
+	github.com/meilisearch/meilisearch-go v0.25.0
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/klauspost/compress v1.15.6 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.37.1-0.20220607072126-8a320890c08d // indirect
+)
---
+++ search/meilisearch/i18n/translation.go
@@ -0,0 +1,15 @@
+package i18n
+
+const (
+	InfoName        = "plugin.meilisearch.info.name"
+	InfoDescription = "plugin.meilisearch.info.name.description"
+
+	ConfigHostTitle         = "plugin.meilisearch.config.host.title"
+	ConfigHostDescription   = "plugin.meilisearch.config.host.description"
+	ConfigIndexTitle        = "plugin.meilisearch.config.index.title"
+	ConfigIndexDescription  = "plugin.meilisearch.config.index.description"
+	ConfigAsyncTitle        = "plugin.meilisearch.config.async.title"
+	ConfigAsyncDescription  = "plugin.meilisearch.config.async.description"
+	ConfigApiKeyTitle       = "plugin.meilisearch.config.api_key.title"
+	ConfigApiKeyDescription = "plugin.meilisearch.config.api_key.description"
+)
