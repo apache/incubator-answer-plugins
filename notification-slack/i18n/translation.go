b3f12a8f9b5d98b53e01b22a8d75359425c4b97a
LinkinStars <linkinstar@foxmail.com>
2024-02-23 11:32:50 +0800

    feat(notification-slack): add slack notification plugin

---
+++ notification-slack/i18n/translation.go
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package i18n
+
+const (
+	InfoName                      = "plugin.slack_notification.backend.info.name"
+	InfoDescription               = "plugin.slack_notification.backend.info.description"
+	ConfigTipTitle                = "plugin.slack_notification.backend.config.tip.title"
+	ConfigNotificationLabel       = "plugin.slack_notification.backend.config.notification.label"
+	ConfigNotificationTitle       = "plugin.slack_notification.backend.config.notification.title"
+	ConfigNotificationDescription = "plugin.slack_notification.backend.config.notification.description"
+
+	UserConfigWebhookURLTitle               = "plugin.slack_notification.backend.user_config.webhook_url.title"
+	UserConfigInboxNotificationsTitle       = "plugin.slack_notification.backend.user_config.inbox_notifications.title"
+	UserConfigInboxNotificationsLabel       = "plugin.slack_notification.backend.user_config.inbox_notifications.label"
+	UserConfigInboxNotificationsDescription = "plugin.slack_notification.backend.user_config.inbox_notifications.description"
+
+	UserConfigAllNewQuestionsNotificationsTitle       = "plugin.slack_notification.backend.user_config.all_new_questions.title"
+	UserConfigAllNewQuestionsNotificationsLabel       = "plugin.slack_notification.backend.user_config.all_new_questions.label"
+	UserConfigAllNewQuestionsNotificationsDescription = "plugin.slack_notification.backend.user_config.all_new_questions.description"
+
+	UserConfigNewQuestionsForFollowingTagsTitle       = "plugin.slack_notification.backend.user_config.new_questions_for_following_tags.title"
+	UserConfigNewQuestionsForFollowingTagsLabel       = "plugin.slack_notification.backend.user_config.new_questions_for_following_tags.label"
+	UserConfigNewQuestionsForFollowingTagsDescription = "plugin.slack_notification.backend.user_config.new_questions_for_following_tags.description"
+
+	TplUpdateQuestion     = "plugin.slack_notification.backend.tpl.update_question"
+	TplAnswerTheQuestion  = "plugin.slack_notification.backend.tpl.answer_the_question"
+	TplUpdateAnswer       = "plugin.slack_notification.backend.tpl.update_answer"
+	TplAcceptAnswer       = "plugin.slack_notification.backend.tpl.accept_answer"
+	TplCommentQuestion    = "plugin.slack_notification.backend.tpl.comment_question"
+	TplCommentAnswer      = "plugin.slack_notification.backend.tpl.comment_answer"
+	TplReplyToYou         = "plugin.slack_notification.backend.tpl.reply_to_you"
+	TplMentionYou         = "plugin.slack_notification.backend.tpl.mention_you"
+	TplInvitedYouToAnswer = "plugin.slack_notification.backend.tpl.invited_you_to_answer"
+	TplNewQuestion        = "plugin.slack_notification.backend.tpl.new_question"
+)
