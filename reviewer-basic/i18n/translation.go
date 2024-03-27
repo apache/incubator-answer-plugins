d4ea4d04a9ecc9d5a461f7363493e1f4615d4ddf
kumfo <kumfo@sifou.com>
2024-03-27 17:48:19 +0800

    feat(review): Improve basic review plugin

--- reviewer-basic/i18n/translation.go
+++ reviewer-basic/i18n/translation.go
@@ -20,10 +20,15 @@
 package i18n
 
 const (
-	InfoName                    = "plugin.basic_reviewer.backend.info.name"
-	InfoDescription             = "plugin.basic_reviewer.backend.info.description"
-	ConfigReviewPostLabel       = "plugin.basic_reviewer.backend.config.review_post.label"
-	ConfigReviewPostTitle       = "plugin.basic_reviewer.backend.config.review_post.title"
-	ConfigReviewPostDescription = "plugin.basic_reviewer.backend.config.review_post.description"
-	CommentNeedReview           = "plugin.basic_reviewer.backend.comment.need_review"
+	InfoName                            = "plugin.basic_reviewer.backend.info.name"
+	InfoDescription                     = "plugin.basic_reviewer.backend.info.description"
+	ConfigReviewPostLabel               = "plugin.basic_reviewer.backend.config.review_post.label"
+	ConfigReviewPostTitle               = "plugin.basic_reviewer.backend.config.review_post.title"
+	ConfigReviewPostDescription         = "plugin.basic_reviewer.backend.config.review_post.description"
+	ConfigReviewPostKeywordsTitle       = "plugin.basic_reviewer.backend.config.review_post_keywords.title"
+	ConfigReviewPostKeywordsDescription = "plugin.basic_reviewer.backend.config.review_post_keywords.description"
+	ConfigDisallowedKeywordsTitle       = "plugin.basic_reviewer.backend.config.disallowed_post_keywords.title"
+	ConfigDisallowedKeywordsDescription = "plugin.basic_reviewer.backend.config.disallowed_post_keywords.description"
+	CommentNeedReview                   = "plugin.basic_reviewer.backend.comment.need_review"
+	CommentMatchWordReview              = "plugin.basic_reviewer.backend.comment.match_word_review"
 )
