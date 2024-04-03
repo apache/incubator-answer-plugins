1904ce2fb4c5384f5cb33a19d99d9dffc8355956
LinkinStars <linkinstar@foxmail.com>
2024-04-03 10:41:15 +0800

    feat(reviewer-akismet): add akismet reviewer

---
+++ reviewer-akismet/akismet.go
@@ -0,0 +1,48 @@
+package basic
+
+import (
+	"github.com/apache/incubator-answer/plugin"
+	"github.com/go-resty/resty/v2"
+	"github.com/segmentfault/pacman/log"
+)
+
+const (
+	commentCheckURL = "https://rest.akismet.com/1.1/comment-check"
+)
+
+func (r *Reviewer) RequestAkismetToCheck(content *plugin.ReviewContent) (isSpam bool, err error) {
+	req := make(map[string]string)
+	req["blog"] = plugin.SiteURL()
+	req["user_ip"] = content.IP
+	req["user_agent"] = content.UserAgent
+	req["comment_content"] = content.Title + "\n" + content.Content
+	req["comment_type"] = "comment"
+	req["is_test"] = "false"
+
+	log.Debugf("request akismet: %+v", req)
+
+	req["api_key"] = r.Config.APIKey
+
+	client := resty.New()
+	resp, err := client.R().
+		SetHeader("Content-Type", "application/x-www-form-urlencoded").
+		SetFormData(req).
+		Post(commentCheckURL)
+
+	if err != nil {
+		log.Errorf("request akismet failed: %v", err)
+		return false, err
+	}
+
+	if resp.StatusCode() != 200 {
+		log.Errorf("request akismet failed: %v", resp.String())
+		return false, nil
+	}
+
+	log.Debugf("akismet response: %v, content title is %s", resp.String(), content.Title)
+
+	if resp.String() == "true" {
+		return true, nil
+	}
+	return false, nil
+}
---
+++ reviewer-akismet/basic.go
@@ -0,0 +1,134 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package basic
+
+import (
+	"encoding/json"
+
+	"github.com/apache/incubator-answer-plugins/reviewer-akismet/i18n"
+	"github.com/apache/incubator-answer/plugin"
+	myI18n "github.com/segmentfault/pacman/i18n"
+	"github.com/segmentfault/pacman/log"
+)
+
+type Reviewer struct {
+	Config *ReviewerConfig
+}
+
+type ReviewerConfig struct {
+	APIKey        string `json:"api_key"`
+	SpamFiltering string `json:"span_filtering"`
+}
+
+func init() {
+	plugin.Register(&Reviewer{
+		Config: &ReviewerConfig{},
+	})
+}
+
+func (r *Reviewer) Info() plugin.Info {
+	return plugin.Info{
+		Name:        plugin.MakeTranslator(i18n.InfoName),
+		SlugName:    "akismet_reviewer",
+		Description: plugin.MakeTranslator(i18n.InfoDescription),
+		Author:      "answerdev",
+		Version:     "1.0.0",
+		Link:        "https://github.com/apache/incubator-answer-plugins/tree/main/reviewer-akismet",
+	}
+}
+
+func (r *Reviewer) Review(content *plugin.ReviewContent) (result *plugin.ReviewResult) {
+	result = &plugin.ReviewResult{Approved: true}
+	if len(r.Config.APIKey) == 0 {
+		return result
+	}
+	// If the author is admin, no need to review
+	if content.Author.Role > 1 {
+		return result
+	}
+
+	isSpam, err := r.RequestAkismetToCheck(content)
+	if err != nil {
+		log.Errorf("Request Akismet to check failed: %v", err)
+		return &plugin.ReviewResult{
+			Approved:     false,
+			ReviewStatus: plugin.ReviewStatusNeedReview,
+			Reason:       plugin.TranslateWithData(myI18n.Language(content.Language), i18n.CommentNeedReview, nil),
+		}
+	}
+	if !isSpam {
+		return result
+	}
+
+	if r.Config.SpamFiltering == "delete" {
+		return &plugin.ReviewResult{
+			Approved:     false,
+			ReviewStatus: plugin.ReviewStatusDeleteDirectly,
+			Reason:       plugin.TranslateWithData(myI18n.Language(content.Language), i18n.CommentNeedReview, nil),
+		}
+	}
+	return &plugin.ReviewResult{
+		Approved:     false,
+		ReviewStatus: plugin.ReviewStatusNeedReview,
+		Reason:       plugin.TranslateWithData(myI18n.Language(content.Language), i18n.CommentNeedReview, nil),
+	}
+}
+
+func (r *Reviewer) ConfigFields() []plugin.ConfigField {
+	return []plugin.ConfigField{
+		{
+			Name:        "api_key",
+			Type:        plugin.ConfigTypeInput,
+			Title:       plugin.MakeTranslator(i18n.ConfigAPIKeyTitle),
+			Description: plugin.MakeTranslator(i18n.ConfigAPIKeyDescription),
+			Required:    false,
+			UIOptions: plugin.ConfigFieldUIOptions{
+				InputType: plugin.InputTypeText,
+				Label:     plugin.MakeTranslator(i18n.ConfigAPIKeyLabel),
+			},
+			Value: r.Config.APIKey,
+		},
+		{
+			Name:      "span_filtering",
+			Type:      plugin.ConfigTypeSelect,
+			Title:     plugin.MakeTranslator(i18n.ConfigSpanFilteringTitle),
+			Required:  false,
+			UIOptions: plugin.ConfigFieldUIOptions{},
+			Value:     r.Config.SpamFiltering,
+			Options: []plugin.ConfigFieldOption{
+				{
+					Value: "review",
+					Label: plugin.MakeTranslator(i18n.ConfigSpanFilteringReview),
+				},
+				{
+					Value: "delete",
+					Label: plugin.MakeTranslator(i18n.ConfigSpanFilteringDelete),
+				},
+			},
+		},
+	}
+}
+
+func (r *Reviewer) ConfigReceiver(config []byte) error {
+	c := &ReviewerConfig{}
+	_ = json.Unmarshal(config, c)
+	r.Config = c
+	return nil
+}
