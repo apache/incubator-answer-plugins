64b6be34b1108538ca0dc337d83b2c982583623c
shuai <lishuailing@sifou.com>
2024-04-09 11:47:05 +0800

    feat: add captcha plugin

---
+++ captcha-basic/i18n/translation.go
@@ -0,0 +1,6 @@
+package i18n
+
+const (
+	InfoName        = "plugin.captcha_basic.backend.info.name"
+	InfoDescription = "plugin.captcha_basic.backend.info.description"
+)
---
+++ captcha-google-v2/common.ts
@@ -0,0 +1,12 @@
+/**
+ * @description: System language conversion bit google reacptcah corresponding language format
+ * @link: https://developers.google.com/recaptcha/docs/language
+ */
+export const languageKeys = {
+  af_ZA: 'af',
+  ar_SA: 'ar',
+  az_AZ: 'az',
+  en_US: 'en',
+  zh_CN: 'zh-CN',
+  zh_TW: 'zh-TW',
+};
