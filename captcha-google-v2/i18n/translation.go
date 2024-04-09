396cec9c56b04bf80edf0b124f123813eb3eaccf
LinkinStars <linkinstar@foxmail.com>
2024-04-09 12:26:26 +0800

    feat(captcha-google-v2): add captcha-google-v2 plugin

--- captcha-google-v2/i18n/translation.go
+++ captcha-google-v2/i18n/translation.go
@@ -1,6 +1,29 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package i18n
 
 const (
-	InfoName        = "plugin.captcha_google_v2.backend.info.name"
-	InfoDescription = "plugin.captcha_google_v2.backend.info.description"
+	InfoName                   = "plugin.captcha_google_v2.backend.info.name"
+	InfoDescription            = "plugin.captcha_google_v2.backend.info.description"
+	ConfigSiteKeyTitle         = "plugin.captcha_google_v2.backend.config.site_key.title"
+	ConfigSiteKeyDescription   = "plugin.captcha_google_v2.backend.config.site_key.description"
+	ConfigSecretKeyTitle       = "plugin.captcha_google_v2.backend.config.secret_key.title"
+	ConfigSecretKeyDescription = "plugin.captcha_google_v2.backend.config.secret_key.description"
 )
