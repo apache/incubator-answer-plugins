a04c63cdd25845a58c1913cc74ca07e0746898aa
kumfo <kumfo@sifou.com>
2024-05-27 14:34:58 +0800

    feat(embed): add basic embed plugin

---
+++ embed-basic/i18n/translation.go
@@ -0,0 +1,36 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package i18n
+
+const (
+	InfoName                = "plugin.basic_embed.backend.info.name"
+	InfoDescription         = "plugin.basic_embed.backend.info.description"
+	ConfigEmbedsTitle       = "plugin.basic_embed.backend.config.embeds.title"
+	ConfigEmbedsDescription = "plugin.basic_embed.backend.config.embeds.description"
+	ConfigOptionCodepen     = "plugin.basic_embed.backend.config.options.codepen"
+	ConfigOptionDropbox     = "plugin.basic_embed.backend.config.options.dropbox"
+	ConfigOptionExcalidraw  = "plugin.basic_embed.backend.config.options.excalidraw"
+	ConfigOptionFigma       = "plugin.basic_embed.backend.config.options.figma"
+	ConfigOptionGithubgist  = "plugin.basic_embed.backend.config.options.githubgist"
+	ConfigOptionJsfiddle    = "plugin.basic_embed.backend.config.options.jsfiddle"
+	ConfigOptionLoom        = "plugin.basic_embed.backend.config.options.loom"
+	ConfigOptionTwitter     = "plugin.basic_embed.backend.config.options.twitter"
+	ConfigOptionYoutube     = "plugin.basic_embed.backend.config.options.youtube"
+)
