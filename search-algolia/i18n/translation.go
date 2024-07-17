0536f157d4228ffcee739f5a1be4ee201fe3ac31
kumfo <kumfo@sifou.com>
2024-07-17 18:29:05 +0800

    fix(translate): fixed

--- search-algolia/i18n/translation.go
+++ search-algolia/i18n/translation.go
@@ -20,22 +20,22 @@
 package i18n
 
 const (
-	InfoName        = "plugin.algolia_search.backend.info.name"
-	InfoDescription = "plugin.algolia_search.backend.info.description"
+	InfoName        = "plugin.algolia-search.backend.info.name"
+	InfoDescription = "plugin.algolia-search.backend.info.description"
 
-	ConfigAPPIDTitle       = "plugin.algolia_search.backend.config.app_id.title"
-	ConfigAPPIDDescription = "plugin.algolia_search.backend.config.app_id.description"
+	ConfigAPPIDTitle       = "plugin.algolia-search.backend.config.app_id.title"
+	ConfigAPPIDDescription = "plugin.algolia-search.backend.config.app_id.description"
 
-	ConfigPublicAPIKeyTitle       = "plugin.algolia_search.backend.config.public_api_key.title"
-	ConfigPublicAPIKeyDescription = "plugin.algolia_search.backend.config.public_api_key.description"
+	ConfigPublicAPIKeyTitle       = "plugin.algolia-search.backend.config.public_api_key.title"
+	ConfigPublicAPIKeyDescription = "plugin.algolia-search.backend.config.public_api_key.description"
 
-	ConfigAPIKeyTitle       = "plugin.algolia_search.backend.config.api_key.title"
-	ConfigAPIKeyDescription = "plugin.algolia_search.backend.config.api_key.description"
+	ConfigAPIKeyTitle       = "plugin.algolia-search.backend.config.api_key.title"
+	ConfigAPIKeyDescription = "plugin.algolia-search.backend.config.api_key.description"
 
-	ConfigIndexTitle       = "plugin.algolia_search.backend.config.index.title"
-	ConfigIndexDescription = "plugin.algolia_search.backend.config.index.description"
+	ConfigIndexTitle       = "plugin.algolia-search.backend.config.index.title"
+	ConfigIndexDescription = "plugin.algolia-search.backend.config.index.description"
 
-	ConfigShowLogoTitle       = "plugin.algolia_search.backend.config.show_logo.title"
-	ConfigShowLogoDescription = "plugin.algolia_search.backend.config.show_logo.description"
-	ConfigShowLogoLabel       = "plugin.algolia_search.backend.config.show_logo.label"
+	ConfigShowLogoTitle       = "plugin.algolia-search.backend.config.show_logo.title"
+	ConfigShowLogoDescription = "plugin.algolia-search.backend.config.show_logo.description"
+	ConfigShowLogoLabel       = "plugin.algolia-search.backend.config.show_logo.label"
 )
