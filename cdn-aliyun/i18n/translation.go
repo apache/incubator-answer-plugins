bf170798a14227eb03b20d73a8f24ded3301ddbe
kumfo <kumfo@sifou.com>
2024-07-19 17:02:29 +0800

    fix(cdn): change namespace

---
+++ cdn-aliyun/i18n/translation.go
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package i18n
+
+const (
+	InfoName        = "plugin.aliyun_cdn.backend.info.name"
+	InfoDescription = "plugin.aliyun_cdn.backend.info.description"
+
+	ConfigEndpointTitle              = "plugin.aliyun_cdn.backend.config.endpoint.title"
+	ConfigEndpointDescription        = "plugin.aliyun_cdn.backend.config.endpoint.description"
+	ConfigBucketNameTitle            = "plugin.aliyun_cdn.backend.config.bucket_name.title"
+	ConfigBucketNameDescription      = "plugin.aliyun_cdn.backend.config.bucket_name.description"
+	ConfigObjectKeyPrefixTitle       = "plugin.aliyun_cdn.backend.config.object_key_prefix.title"
+	ConfigObjectKeyPrefixDescription = "plugin.aliyun_cdn.backend.config.object_key_prefix.description"
+	ConfigAccessKeyIdTitle           = "plugin.aliyun_cdn.backend.config.access_key_id.title"
+	ConfigAccessKeyIdDescription     = "plugin.aliyun_cdn.backend.config.access_key_id.description"
+	ConfigAccessKeySecretTitle       = "plugin.aliyun_cdn.backend.config.access_key_secret.title"
+	ConfigAccessKeySecretDescription = "plugin.aliyun_cdn.backend.config.access_key_secret.description"
+	ConfigVisitUrlPrefixTitle        = "plugin.aliyun_cdn.backend.config.visit_url_prefix.title"
+	ConfigVisitUrlPrefixDescription  = "plugin.aliyun_cdn.backend.config.visit_url_prefix.description"
+	ConfigMaxFileSizeTitle           = "plugin.aliyun_cdn.backend.config.max_file_size.title"
+	ConfigMaxFileSizeDescription     = "plugin.aliyun_cdn.backend.config.max_file_size.description"
+
+	ErrMisStorageConfig    = "plugin.aliyun_cdn.backend.err.mis_storage_config"
+	ErrUnsupportedFileType = "plugin.aliyun_cdn.backend.err.unsupported_file_type"
+	ErrOverFileSizeLimit   = "plugin.aliyun_cdn.backend.err.over_file_size_limit"
+	ErrUploadFileFailed    = "plugin.aliyun_cdn.backend.err.upload_file_failed"
+)
