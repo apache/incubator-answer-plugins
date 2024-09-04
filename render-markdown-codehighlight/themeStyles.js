1e7fd0968da677a16c6c5563582e33089b68223f
Chen <2259503767@qq.com>
2024-09-04 16:55:05 +0800

    feat: codehight frontend function

---
+++ render-markdown-codehighlight/themeStyles.js
@@ -0,0 +1,230 @@
+export const themeStyles = {
+  "1c": {
+    light: () => import('highlight.js/styles/1c-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/a11y-dark.min.css?inline')
+  },
+  "a11y": {
+    light: () => import('highlight.js/styles/a11y-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/a11y-dark.min.css?inline')
+  },
+  "agate": {
+    light: () => import('highlight.js/styles/1c-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/agate.min.css?inline')
+  },
+  "an": {
+    light: () => import('highlight.js/styles/1c-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/an-old-hope.min.css?inline')
+  },
+  "androidstudio": {
+    light: () => import('highlight.js/styles/1c-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/androidstudio.min.css?inline')
+  },
+  "arduino": {
+    light: () => import('highlight.js/styles/arduino-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/a11y-dark.min.css?inline')
+  },
+  "arta": {
+    light: () => import('highlight.js/styles/1c-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/arta.min.css?inline')
+  },
+  "ascetic": {
+    light: () => import('highlight.js/styles/ascetic.min.css?inline'),
+    dark: () => import('highlight.js/styles/a11y-dark.min.css?inline')
+  },
+  "atom": {
+    light: () => import('highlight.js/styles/atom-one-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/atom-one-dark.min.css?inline')
+  },
+  "brown": {
+    light: () => import('highlight.js/styles/brown-paper.min.css?inline'),
+    dark: () => import('highlight.js/styles/a11y-dark.min.css?inline')
+  },
+  "codepen": {
+    light: () => import('highlight.js/styles/1c-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/codepen-embed.min.css?inline')
+  },
+  "color": {
+    light: () => import('highlight.js/styles/color-brewer.min.css?inline'),
+    dark: () => import('highlight.js/styles/a11y-dark.min.css?inline')
+  },
+  "dark": {
+    light: () => import('highlight.js/styles/1c-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/dark.min.css?inline')
+  },
+  "default": {
+    light: () => import('highlight.js/styles/default.min.css?inline'),
+    dark: () => import('highlight.js/styles/a11y-dark.min.css?inline')
+  },
+  "devibeans": {
+    light: () => import('highlight.js/styles/1c-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/devibeans.min.css?inline')
+  },
+  "docco": {
+    light: () => import('highlight.js/styles/docco.min.css?inline'),
+    dark: () => import('highlight.js/styles/a11y-dark.min.css?inline')
+  },
+  "far": {
+    light: () => import('highlight.js/styles/1c-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/far.min.css?inline')
+  },
+  "felipec": {
+    light: () => import('highlight.js/styles/1c-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/felipec.min.css?inline')
+  },
+  "foundation": {
+    light: () => import('highlight.js/styles/foundation.min.css?inline'),
+    dark: () => import('highlight.js/styles/a11y-dark.min.css?inline')
+  },
+  "github": {
+    light: () => import('highlight.js/styles/github.min.css?inline'),
+    dark: () => import('highlight.js/styles/github-dark.min.css?inline')
+  },
+  "gml": {
+    light: () => import('highlight.js/styles/1c-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/gml.min.css?inline')
+  },
+  "googlecode": {
+    light: () => import('highlight.js/styles/googlecode.min.css?inline'),
+    dark: () => import('highlight.js/styles/a11y-dark.min.css?inline')
+  },
+  "gradient": {
+    light: () => import('highlight.js/styles/gradient-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/gradient-dark.min.css?inline')
+  },
+  "grayscale": {
+    light: () => import('highlight.js/styles/grayscale.min.css?inline'),
+    dark: () => import('highlight.js/styles/a11y-dark.min.css?inline')
+  },
+  "hybrid": {
+    light: () => import('highlight.js/styles/1c-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/hybrid.min.css?inline')
+  },
+  "idea": {
+    light: () => import('highlight.js/styles/idea.min.css?inline'),
+    dark: () => import('highlight.js/styles/a11y-dark.min.css?inline')
+  },
+  "intellij": {
+    light: () => import('highlight.js/styles/intellij-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/a11y-dark.min.css?inline')
+  },
+  "ir": {
+    light: () => import('highlight.js/styles/1c-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/ir-black.min.css?inline')
+  },
+  "isbl": {
+    light: () => import('highlight.js/styles/isbl-editor-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/isbl-editor-dark.min.css?inline')
+  },
+  "kimbie": {
+    light: () => import('highlight.js/styles/kimbie-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/kimbie-dark.min.css?inline')
+  },
+  "lightfair": {
+    light: () => import('highlight.js/styles/lightfair.min.css?inline'),
+    dark: () => import('highlight.js/styles/a11y-dark.min.css?inline')
+  },
+  "lioshi": {
+    light: () => import('highlight.js/styles/1c-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/lioshi.min.css?inline')
+  },
+  "magula": {
+    light: () => import('highlight.js/styles/magula.min.css?inline'),
+    dark: () => import('highlight.js/styles/a11y-dark.min.css?inline')
+  },
+  "mono": {
+    light: () => import('highlight.js/styles/mono-blue.min.css?inline'),
+    dark: () => import('highlight.js/styles/a11y-dark.min.css?inline')
+  },
+  "monokai": {
+    light: () => import('highlight.js/styles/1c-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/monokai.min.css?inline')
+  },
+  "night": {
+    light: () => import('highlight.js/styles/1c-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/night-owl.min.css?inline')
+  },
+  "nnfx": {
+    light: () => import('highlight.js/styles/nnfx-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/nnfx-dark.min.css?inline')
+  },
+  "nord": {
+    light: () => import('highlight.js/styles/1c-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/nord.min.css?inline')
+  },
+  "obsidian": {
+    light: () => import('highlight.js/styles/1c-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/obsidian.min.css?inline')
+  },
+  "panda": {
+    light: () => import('highlight.js/styles/panda-syntax-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/panda-syntax-dark.min.css?inline')
+  },
+  "paraiso": {
+    light: () => import('highlight.js/styles/paraiso-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/paraiso-dark.min.css?inline')
+  },
+  "pojoaque": {
+    light: () => import('highlight.js/styles/pojoaque.min.css?inline'),
+    dark: () => import('highlight.js/styles/a11y-dark.min.css?inline')
+  },
+  "purebasic": {
+    light: () => import('highlight.js/styles/purebasic.min.css?inline'),
+    dark: () => import('highlight.js/styles/a11y-dark.min.css?inline')
+  },
+  "qtcreator": {
+    light: () => import('highlight.js/styles/qtcreator-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/qtcreator-dark.min.css?inline')
+  },
+  "rainbow": {
+    light: () => import('highlight.js/styles/1c-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/rainbow.min.css?inline')
+  },
+  "routeros": {
+    light: () => import('highlight.js/styles/routeros.min.css?inline'),
+    dark: () => import('highlight.js/styles/a11y-dark.min.css?inline')
+  },
+  "school": {
+    light: () => import('highlight.js/styles/school-book.min.css?inline'),
+    dark: () => import('highlight.js/styles/a11y-dark.min.css?inline')
+  },
+  "shades": {
+    light: () => import('highlight.js/styles/1c-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/shades-of-purple.min.css?inline')
+  },
+  "srcery": {
+    light: () => import('highlight.js/styles/1c-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/srcery.min.css?inline')
+  },
+  "stackoverflow": {
+    light: () => import('highlight.js/styles/stackoverflow-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/stackoverflow-dark.min.css?inline')
+  },
+  "sunburst": {
+    light: () => import('highlight.js/styles/1c-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/sunburst.min.css?inline')
+  },
+  "tokyo": {
+    light: () => import('highlight.js/styles/tokyo-night-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/tokyo-night-dark.min.css?inline')
+  },
+  "tomorrow": {
+    light: () => import('highlight.js/styles/1c-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/tomorrow-night-bright.min.css?inline')
+  },
+  "vs": {
+    light: () => import('highlight.js/styles/vs.min.css?inline'),
+    dark: () => import('highlight.js/styles/a11y-dark.min.css?inline')
+  },
+  "vs2015": {
+    light: () => import('highlight.js/styles/1c-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/vs2015.min.css?inline')
+  },
+  "xcode": {
+    light: () => import('highlight.js/styles/xcode.min.css?inline'),
+    dark: () => import('highlight.js/styles/a11y-dark.min.css?inline')
+  },
+  "xt256": {
+    light: () => import('highlight.js/styles/1c-light.min.css?inline'),
+    dark: () => import('highlight.js/styles/xt256.min.css?inline')
+  }
+};
